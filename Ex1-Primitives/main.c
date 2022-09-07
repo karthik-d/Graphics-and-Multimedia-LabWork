a773266503b0f3c02905bd8598982a803c5fd994
karthik-d <karthikdesingu2000@gmail.com>
2022-09-08 00:17:16 +0530

    vertex highlight

--- Ex1-Primitives/main.c
+++ Ex1-Primitives/main.c
@@ -6,69 +6,112 @@
 
 void plotPoints()   {
     glBegin(GL_POINTS);
-    glVertex2d(150,100);
-    glVertex2d(100,230);
-    glVertex2d(170,130);
-    glVertex2d(300,350);
+    // line
+    glVertex2d(20, 30);
+    glVertex2d(130, 40);
+    glVertex2d(170, 50);
+    glVertex2d(120, 140);
+    // line strip
+    glVertex2d(20, 190);
+    glVertex2d(130, 200);
+    glVertex2d(50, 230);
+    glVertex2d(120, 300);
+    // line loop
+    glVertex2d(20, 350);
+    glVertex2d(130, 360);
+    glVertex2d(170, 390);
+    glVertex2d(120, 460);
+    // triangle
+    glVertex2d(230, 350);
+    glVertex2d(230, 450);
+    glVertex2d(370, 400);
+    // triangle strip
+    glVertex2d(230, 190);
+    glVertex2d(230, 240);
+    glVertex2d(380, 210);
+    glVertex2d(300, 280);
+    // triangle fan
+    glVertex2d(230, 30);
+    glVertex2d(230, 120);
+    glVertex2d(380, 150);
+    glVertex2d(300, 30);
+    // quads
+    glVertex2d(500, 450);
+    glVertex2d(400, 400);
+    glVertex2d(450, 350);
+    glVertex2d(600, 400);
+    // quad strip
+    glVertex2d(500, 320);
+    glVertex2d(400, 290);
+    glVertex2d(450, 190);
+    glVertex2d(600, 240);
+    glVertex2d(620, 350);
+    // polygon
+    glVertex2d(480, 160);
+    glVertex2d(560, 160);
+    glVertex2d(600, 70);
+    glVertex2d(560, 40);
+    glVertex2d(480, 40);
+    glVertex2d(410, 90);
     glEnd();
 }
 
 
 void plotLines()    {
     glBegin(GL_LINES);
-    glVertex2d(150,100);
-    glVertex2d(100,230);
-    glVertex2d(170,130);
-    glVertex2d(300,350);
+    glVertex2d(20, 30);
+    glVertex2d(130, 40);
+    glVertex2d(170, 50);
+    glVertex2d(120, 140);
     glEnd();
 }
 
 
 void plotLineStrip()    {
     glBegin(GL_LINE_STRIP);
-    glVertex2d(150,100);
-    glVertex2d(100,230);
-    glVertex2d(170,130);
-    glVertex2d(300,350);
+    glVertex2d(20, 190);
+    glVertex2d(130, 200);
+    glVertex2d(50, 230);
+    glVertex2d(120, 300);
     glEnd();
 }
 
 
 void plotLineLoop() {
     glBegin(GL_LINE_LOOP);
-    glVertex2d(150,100);
-    glVertex2d(100,230);
-    glVertex2d(170,130);
-    glVertex2d(300,350);
+    glVertex2d(20, 350);
+    glVertex2d(130, 360);
+    glVertex2d(170, 390);
+    glVertex2d(120, 460);
     glEnd();
 }
 
 
 void plotTriangles()    {
     glBegin(GL_TRIANGLES);
-    glVertex2d(150,100);
-    glVertex2d(100,230);
-    glVertex2d(170,130);
+    glVertex2d(230, 350);
+    glVertex2d(230, 450);
+    glVertex2d(370, 400);
     glEnd();
 }
 
 
 void plotTriangleStrip()    {
     glBegin(GL_TRIANGLE_STRIP);
-    glVertex2d(150,100);
-    glVertex2d(100,230);
-    glVertex2d(170,130);
-    glVertex2d(300,100);
+    glVertex2d(230, 190);
+    glVertex2d(230, 240);
+    glVertex2d(380, 210);
+    glVertex2d(300, 280);
     glEnd();
 }
 
 
 void plotTriangleFan()  {
     glBegin(GL_TRIANGLE_FAN);
-    glVertex2d(500, 50);
-    glVertex2d(400, 100);
-    glVertex2d(450, 200);
-    glVertex2d(600, 180);
+    glVertex2d(230, 30);
+    glVertex2d(230, 120);
+    glVertex2d(380, 150);
+    glVertex2d(300, 30);
     glEnd();
 }
 
@@ -87,8 +130,8 @@ void plotQuadStrip()    {
     glBegin(GL_QUAD_STRIP);
     glVertex2d(500, 320);
     glVertex2d(400, 290);
-    glVertex2d(450, 250);
-    glVertex2d(600, 300);
+    glVertex2d(450, 190);
+    glVertex2d(600, 240);
     glVertex2d(620, 350);
     glEnd();
 }
@@ -109,12 +152,12 @@ void plotPolygon()    {
 void display()    {
     glClear(GL_COLOR_BUFFER_BIT);
     plotPoints();
-    // plotLines();
-    // plotLineStrip();
-    // plotLineLoop();
-    // plotTriangles();
-    // plotTriangleStrip();
-    // plotTriangleFan();
+    plotLines();
+    plotLineStrip();
+    plotLineLoop();
+    plotTriangles();
+    plotTriangleStrip();
+    plotTriangleFan();
     plotQuads();
     plotQuadStrip();
     plotPolygon();
