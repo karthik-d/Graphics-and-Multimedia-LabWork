75cb76e06e2e9e306848ed8bf26e8a1527b98398
karthik-d <karthikdesingu2000@gmail.com>
2022-09-08 13:20:34 +0530

    coordinated marked

--- Ex2-DDALineDrawing/main.c
+++ Ex2-DDALineDrawing/main.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <math.h>
 
+#define BUFFER_SIZE 100
+
+
 void plotDivisionLines()    {
     glBegin(GL_LINES);
     glVertex2d(-320, 0);
@@ -9,6 +12,24 @@ void plotDivisionLines()    {
     glVertex2d(0, -240);
     glVertex2d(0, 240);
     glEnd();
+}
+
+
+void renderSpacedBitmapString(float x, float y, void *font, char *string) {
+    char *c;
+    int x1 = x;
+    for (c = string; *c != '\0'; c++) {
+        glRasterPos2f(x1, y);
+        glutBitmapCharacter(font, *c);
+        x1 = x1 + glutBitmapWidth(font, *c);
+    }
+}
+
+
+void markString(char *string, int x, int y, int x_offset, int y_offset) {
+    glColor3f(255.0, 0, 0.0); // red color
+    renderSpacedBitmapString(x+x_offset, y+y_offset, GLUT_BITMAP_HELVETICA_10, string);
+    glFlush();
 }
 
 
@@ -24,8 +45,8 @@ void plotLineDDA(int start_x, int start_y, int end_x, int end_y)  {
     float slope = (float) (end_y - start_y)/(end_x - start_x);
     float abs_slope = slope;
     
-    short dx_sign = (start_x <= end_x ? 1 : -1); 
-    short dy_sign = (start_y <= end_y ? 1 : -1); 
+    short dx_sign = (start_x <= end_x ? 1 : -1);
+    short dy_sign = (start_y <= end_y ? 1 : -1);
     
     if(slope>0) {
         abs_slope = slope;
@@ -47,13 +68,13 @@ void plotLineDDA(int start_x, int start_y, int end_x, int end_y)  {
         check_x = 1;
     }
 
-    printf("Slope: %f\n", slope);
+    printf("\nslope: %f\n", slope);
     printf("dx: %f, dy: %f\n", dx, dy);
     printf("check_x: %d\n", check_x);
 
     int x_ = start_x;
     int y_ = start_y;
-    float x_val = start_x; 
+    float x_val = start_x;
     float y_val = start_y;
     plotPoint(x_, y_);
     while((check_x && end_x!=x_) || (!check_x && end_y!=y_))    {
@@ -65,6 +86,11 @@ void plotLineDDA(int start_x, int start_y, int end_x, int end_y)  {
         // printf("\nx: %d, y: %d", x_, y_);
         fflush(stdout);
     }
+    char *point_label = (char*)malloc(sizeof(char)*BUFFER_SIZE);
+    sprintf(point_label, "(%d, %d)", start_x, start_y);
+    markString(point_label, start_x, start_y, 20, 0);
+    sprintf(point_label, "(%d, %d)", end_x, end_y);
+    markString(point_label, end_x, end_y, 20, 0);
 }
 
 
@@ -87,11 +113,10 @@ void display()  {
     glFlush();
 }
 
-
 void init() {
     glClearColor(1.0, 1.0, 1.0, 0.0);
     glColor3f(0.0f, 0.0f, 0.0f);
-    glPointSize(10);
+    glPointSize(4);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluOrtho2D(-320.0, 320.0, -240.0, 240.0);
