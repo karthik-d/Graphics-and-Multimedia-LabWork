c28c1568c9a1808d9ac484b254808a543618fd67
karthik-d <karthikdesingu2000@gmail.com>
2022-09-09 22:33:05 +0530

    point-wise labels added

--- Ex1-Primitives/main.c
+++ Ex1-Primitives/main.c
@@ -15,10 +15,11 @@ void renderSpacedBitmapString(float x, float y, void *font, char *string) {
 }
 
 
-void markString(char *string, int x, int y, int x_offset, int y_offset) {
+void markString(char *string, int x, int y, int x_offset, int y_offset, short large) {
+    void *size = (large ? GLUT_BITMAP_HELVETICA_12 : GLUT_BITMAP_HELVETICA_10);
     glColor3f(255.0, 0, 0.0); // red color
     printf("\n%s", string);
-    renderSpacedBitmapString(x+x_offset, y+y_offset, GLUT_BITMAP_HELVETICA_12, string);
+    renderSpacedBitmapString(x+x_offset, y+y_offset, size, string);
     glFlush();
 }
 
@@ -27,18 +28,12 @@ void markString(char *string, int x, int y, int x_offset, int y_offset) {
 // TRIANGLE FAN, QUADS, QUAD_STRIP, POLYGON
 
 void plotPoints()   {
-    char *point_label = (char*)malloc(sizeof(char)*BUFFER_SIZE);
     glBegin(GL_POINTS);
-    // line
-    sprintf(point_label, "(%d, %d)", 20, 30);
+    // line    
     glVertex2d(20, 30);
-    markString(point_label, 20, 30, 0, 0);
     glVertex2d(130, 40);
-    markString("(130, 40)", 130, 40, 5, 5);
     glVertex2d(170, 50);
-    markString("(170, 50)", 170, 50, 5, 5);
     glVertex2d(120, 140);
-    markString("(120, 140)", 120, 140, 5, 5);
     // line strip
     glVertex2d(20, 190);
     glVertex2d(130, 200);
@@ -82,6 +77,72 @@ void plotPoints()   {
     glVertex2d(480, 40);
     glVertex2d(410, 90);
     glEnd();
+}
+
+
+void plotLabels()   {
+    
+    markString("POINTS and LINES", 10, 150, 0, 0, 1);
+    markString("A(20, 30)", 20, 30, -5, 5, 0);
+    markString("B(130, 40)", 130, 40, -5, 5, 0);
+    markString("C(170, 50)", 170, 50, -5, 5, 0);
+    markString("D(120, 140)", 120, 140, -5, 5, 0);
+
+    markString("LINE STRIP", 30, 310, 0, 0, 1);
+    markString("E(20, 190)", 20, 190, -5, 5, 0);
+    markString("F(130, 200)", 130, 200, -5, 5, 0);
+    markString("G(50, 230)", 50, 230, -5, 5, 0);
+    markString("H(120, 300)", 120, 300, -5, 5, 0);
+
+    markString("LINE LOOP", 30, 460, 0, 0, 1);
+    markString("A(20, 350)", 20, 350, -5, 5, 0);
+    markString("B(130, 360)", 130, 360, -5, 5, 0);
+    markString("C(170, 390)", 170, 390, -5, 5, 0);
+    markString("D(120, 460)", 120, 460, -5, 5, 0);
+
+    markString("TRAINGLES", 300, 460, 0, 0, 1);
+    markString("A(230, 350)", 230, 350, 5, -10, 0);
+    markString("B(230, 450)", 230, 450, 5, 5, 0);
+    markString("C(370, 400)", 370, 400, -20, 10, 0);
+
+    markString("TRAINGLE STRIP", 240, 310, 0, 0, 1);
+    markString("A(230, 190)", 230, 190, 5, -10, 0);
+    markString("B(230, 240)", 230, 240, -20, 15, 0);
+    markString("C(380, 210)", 380, 210, -10, 10, 0);
+    markString("D(300, 280)", 300, 280, -20, 10, 0);
+
+    markString("TRAINGLE FAN", 240, 150, 0, 0, 1);
+    markString("A(230, 30)", 230, 30, 5, -10, 0);
+    markString("B(230, 120)", 230, 120, -20, 15, 0);
+    markString("C(380, 150)", 380, 150, -10, 10, 0);
+    markString("D(300, 30)", 300, 30, 10, 10, 0);
+
+    markString("QUADS", 500, 460, 0, 0, 1);
+    markString("A(500, 450)", 500, 450, 20, -5, 0);
+    markString("B(400, 400)", 400, 400, -40, -30, 0);
+    markString("C(450, 350)", 450, 350, 25, 0, 0);
+    markString("D(600, 400)", 600, 400, -20, 10, 0);
+
+    markString("QUAD STRIP", 500, 460, 0, 0, 1);
+    markString("A(500, 320)", 500, 320, 20, -5, 0);
+    markString("B(400, 290)", 400, 290, -40, -30, 0);
+    markString("D(450, 190)", 450, 190, -20, 10, 0);
+    markString("C(480, 270)", 480, 270, 25, 0, 0);
+    markString("D(620, 350)", 620, 350, -20, 10, 0);
+
+    (480, 160);
+    glVertex2d(560, 160);
+    glVertex2d(600, 70);
+    glVertex2d(560, 40);
+    glVertex2d(480, 40);
+    glVertex2d(410, 90);
+    markString("POLYGONS", 500, 180, 0, 0, 1);
+    markString("A(480, 160)", 480, 160, 10, 5, 0);
+    markString("B(560, 160)", 560, 160, 20, -5, 0);
+    markString("C(600, 70)", 600, 70, -15, -20, 0);
+    markString("D(560, 40)", 560, 40, -5, -20, 0);
+    markString("E(480, 40)", 480, 40, 0, -20, 0);
+    markString("F(410, 90)", 410, 90, -25, -30, 0);
 }
 
 
@@ -159,7 +220,7 @@ void plotQuadStrip()    {
     glVertex2d(500, 320);
     glVertex2d(400, 290);
     glVertex2d(450, 190);
-    glVertex2d(600, 240);
+    glVertex2d(480, 270);
     glVertex2d(620, 350);
     glEnd();
 }
@@ -180,6 +241,7 @@ void plotPolygon()    {
 void display()    {
     glClear(GL_COLOR_BUFFER_BIT);
     plotPoints();
+    plotLabels();
     glColor3f(0.0f, 0.0f, 0.0f);
     plotLines();
     plotLineStrip();
@@ -208,7 +270,7 @@ int main(int argc,char* argv[]) {
     glutInit(&argc,argv);
     glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
     glutInitWindowSize(640,480);
-    glutCreateWindow("Ex1 - Primitives and Checkboard");
+    glutCreateWindow("Ex1A - OpenGL Primitives");
     glutDisplayFunc(display);
     init();
     glutMainLoop();
