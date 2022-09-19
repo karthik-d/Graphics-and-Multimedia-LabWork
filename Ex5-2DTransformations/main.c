f006e0f86ed34854b40bc38d06ce60910c687516
karthik-d <karthikdesingu2000@gmail.com>
2022-09-19 17:00:47 +0530

    ex5 templated

---
+++ Ex5-2DTransformations/main.c
@@ -0,0 +1,72 @@
+#include <GL/glut.h>
+#include <stdio.h>
+
+
+void renderSpacedBitmapString(float x, float y, void *font, char *string) {
+    char *c;
+    int x1 = x;
+    for (c = string; *c != '\0'; c++) {
+        glRasterPos2f(x1, y);
+        glutBitmapCharacter(font, *c);
+        x1 = x1 + glutBitmapWidth(font, *c);
+    }
+}
+
+
+void markString(char *string, int x, int y, int x_offset, int y_offset) {
+    glColor3f(255.0, 0, 0.0); // red color
+    renderSpacedBitmapString(x+x_offset, y+y_offset, GLUT_BITMAP_HELVETICA_12, string);
+    glFlush();
+}
+
+
+void plotDivisionLines()    {
+    glBegin(GL_LINES);
+    glVertex2d(-320, 0);
+    glVertex2d(320, 0);
+    glVertex2d(0, -240);
+    glVertex2d(0, 240);
+    glEnd();
+}
+
+
+void plotPoint(int x, int y, int x_offset, int y_offset)    {
+    glBegin(GL_POINTS);
+    glVertex2d(x + x_offset, y + y_offset);
+    glEnd();
+}
+
+
+void display_transforms()   {
+    glClear(GL_COLOR_BUFFER_BIT);
+    plotDivisionLines();
+
+    //
+
+    glFlush();
+}
+
+
+void init() {
+    glClearColor(1.0, 1.0, 1.0, 0.0);
+    glColor3f(0.0f, 0.0f, 0.0f);
+    glPointSize(4);
+    glLineWidth(4);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(-320.0, 320.0, -240.0, 240.0);
+}
+
+
+int main(int argc, char **argv)  {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(640, 480);
+
+    glutCreateWindow("Ex4 - 2D Transformation");
+    glutDisplayFunc(display_transforms);
+
+    init();
+    glutMainLoop();    
+    return 1;
+}
