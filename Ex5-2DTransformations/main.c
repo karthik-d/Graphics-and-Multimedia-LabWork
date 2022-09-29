0f3deebb5156c2fad2e14dc90304fd36879acef0
karthik-d <karthikdesingu2000@gmail.com>
2022-09-29 17:26:40 +0530

    converted to float operations

--- Ex5-2DTransformations/main.c
+++ Ex5-2DTransformations/main.c
@@ -1,6 +1,9 @@
 #include <GL/glut.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+#define PI 3.141592654
 
 
 void renderSpacedBitmapString(float x, float y, void *font, char *string) {
@@ -21,11 +24,11 @@ void markString(char *string, int x, int y, int x_offset, int y_offset) {
 }
 
 
-int** multiplyMatrices(int **m1, int **m2, int r1, int c1, int c2)    {
+float** multiplyMatrices(float **m1, float **m2, int r1, int c1, int c2)    {
     // assume compatible matrices
-    int **res = (int**)malloc(sizeof(int*)*r1);
+    float **res = (float**)malloc(sizeof(float*)*r1);
     for(int i=0; i<r1; i++) {
-        *(res+i) = (int*)malloc(sizeof(int)*c2);
+        *(res+i) = (float*)malloc(sizeof(float)*c2);
         for(int j=0; j<c2; j++) {
             res[i][j] = 0;
             for(int k=0;k<c1;k++)   {
@@ -37,7 +40,7 @@ int** multiplyMatrices(int **m1, int **m2, int r1, int c1, int c2)    {
 }
 
 
-void displayMatrix(int **matrix, int r, int c)  {
+void displayMatrix(float **matrix, int r, int c)  {
     printf("\n");
     for(int i=0;i<r;i++)    {
         for(int j=0; j<c; j++)  {
@@ -74,10 +77,10 @@ void plotTriangle(int *xs, int *ys)   {
 }
 
 
-int** makeTriangleMatrix(int *xs, int *ys)  {
-    int **res = (int**)malloc(sizeof(int*)*3);
+float** makeTriangleMatrix(int *xs, int *ys)  {
+    float **res = (float**)malloc(sizeof(float*)*3);
     for(int i=0; i<3; i++) {
-        *(res+i) = (int*)malloc(sizeof(int)*3);
+        *(res+i) = (float*)malloc(sizeof(float)*3);
     }
     for(int i=0; i<3; i++) {
         res[0][i] = xs[i];
@@ -88,10 +91,10 @@ int** makeTriangleMatrix(int *xs, int *ys)  {
 }
 
 
-int** makeTranslationMatrix(int tx, int ty) {
-    int **res = (int**)malloc(sizeof(int*)*3);
+float** makeTranslationMatrix(int tx, int ty) {
+    float **res = (float**)malloc(sizeof(float*)*3);
     for(int i=0; i<3; i++) {
-        *(res+i) = (int*)malloc(sizeof(int)*3);
+        *(res+i) = (float*)malloc(sizeof(float)*3);
         for(int j=0; j<3; j++) {
             if(i==j){
                 res[i][j] = 1;
@@ -107,12 +110,33 @@ int** makeTranslationMatrix(int tx, int ty) {
 }
 
 
-int** makeReflectionMatrix(short along_x, short along_y, short along_xeqy) {
+float** makeRotationMatrix(int theta) {
+    float **res = (float**)malloc(sizeof(float*)*3);
+    for(int i=0; i<3; i++) {
+        *(res+i) = (float*)malloc(sizeof(float*)*3);
+        for(int j=0; j<3; j++) {
+            if(i==j){
+                res[i][j] = 1;
+            }
+            else{
+                res[i][j] = 0;
+            }
+        }
+    }
+    res[0][0] = cos(theta*PI/180);
+    res[1][1] = res[0][0];
+    res[0][1] = -sin(theta*PI/180);
+    res[1][0] = -res[0][1];
+    return res;
+}
+
+
+float** makeReflectionMatrix(short along_x, short along_y, short along_xeqy) {
     // truth of the first two arguments overrides the last
 
-    int **res = (int**)malloc(sizeof(int*)*3);
+    float **res = (float**)malloc(sizeof(float*)*3);
     for(int i=0; i<3; i++) {
-        *(res+i) = (int*)malloc(sizeof(int)*3);
+        *(res+i) = (float*)malloc(sizeof(float)*3);
         for(int j=0; j<3; j++) {
             if(i==j){
                 res[i][j] = 1;
@@ -132,7 +156,7 @@ int** makeReflectionMatrix(short along_x, short along_y, short along_xeqy) {
         override_xeqy = 1;
     }
     if(!override_xeqy && along_xeqy)    {
-        int *temp = res[0];
+        float *temp = res[0];
         res[0] = res[1];
         res[1] = temp;
     }
@@ -142,11 +166,40 @@ int** makeReflectionMatrix(short along_x, short along_y, short along_xeqy) {
 
 void plotTranslatedTriangle(int *xs, int *ys, int tx, int ty)    {
     glBegin(GL_TRIANGLES);
-    int **tr_matrix = makeTranslationMatrix(tx, ty);
-    int **triangle_matrix = makeTriangleMatrix(xs, ys);
-    int **translated_triangle = multiplyMatrices(tr_matrix, triangle_matrix, 3, 3, 3);
+    float **tr_matrix = makeTranslationMatrix(tx, ty);
+    float **triangle_matrix = makeTriangleMatrix(xs, ys);
+    float **translated_triangle = multiplyMatrices(tr_matrix, triangle_matrix, 3, 3, 3);
     for(int i=0; i<3; i++)  {
-        glVertex2d(translated_triangle[0][i], translated_triangle[1][i]);
+        glVertex2d((int)translated_triangle[0][i], (int)translated_triangle[1][i]);
+    }
+    glEnd();
+}
+
+
+void plotRotatedTriangle(int *xs, int *ys, int xr, int yr, int theta)    {
+    // (xr, yr) --> Pivot Point
+    glBegin(GL_TRIANGLES);
+    
+    float **triangle_matrix = makeTriangleMatrix(xs, ys);
+    displayMatrix(triangle_matrix, 3, 3);
+
+    float **rotation_matrix = makeRotationMatrix(theta);
+    
+    float **rotated_triangle = multiplyMatrices(
+        makeTranslationMatrix(xr, yr),
+        multiplyMatrices(
+            makeRotationMatrix(theta),
+            multiplyMatrices(
+                makeTranslationMatrix(-xr, -yr),
+                triangle_matrix,
+                3, 3, 3
+            ),
+            3, 3, 3
+        ),
+        3, 3, 3);
+    
+    for(int i=0; i<3; i++)  {
+        glVertex2d((int)rotated_triangle[0][i], (int)rotated_triangle[1][i]);
     }
     glEnd();
 }
@@ -155,32 +208,29 @@ void plotTranslatedTriangle(int *xs, int *ys, int tx, int ty)    {
 void plotReflectedTriangle(int *xs, int *ys)    {
     glBegin(GL_TRIANGLES);
     
-    int **xref_matrix = makeReflectionMatrix(1, 0, 0);
-    int **yref_matrix = makeReflectionMatrix(0, 1, 0);
-    int **xyref_matrix = makeReflectionMatrix(1, 1, 0);
-    int **xeqyref_matrix = makeReflectionMatrix(0, 0, 1);
-    displayMatrix(xref_matrix, 3, 3);
+    float **xref_matrix = makeReflectionMatrix(1, 0, 0);
+    float **yref_matrix = makeReflectionMatrix(0, 1, 0);
+    float **xyref_matrix = makeReflectionMatrix(1, 1, 0);
+    float **xeqyref_matrix = makeReflectionMatrix(0, 0, 1);
     
-    int **triangle_matrix = makeTriangleMatrix(xs, ys);
-    displayMatrix(triangle_matrix, 3, 3);
+    float **triangle_matrix = makeTriangleMatrix(xs, ys);
     
-    int **xref_triangle = multiplyMatrices(xref_matrix, triangle_matrix, 3, 3, 3);
-    displayMatrix(xref_triangle, 3, 3);
-    int **yref_triangle = multiplyMatrices(yref_matrix, triangle_matrix, 3, 3, 3);
-    int **xyref_triangle = multiplyMatrices(xyref_matrix, triangle_matrix, 3, 3, 3);
-    int **xeqyref_triangle = multiplyMatrices(xeqyref_matrix, triangle_matrix, 3, 3, 3);
+    float **xref_triangle = multiplyMatrices(xref_matrix, triangle_matrix, 3, 3, 3);
+    float **yref_triangle = multiplyMatrices(yref_matrix, triangle_matrix, 3, 3, 3);
+    float **xyref_triangle = multiplyMatrices(xyref_matrix, triangle_matrix, 3, 3, 3);
+    float **xeqyref_triangle = multiplyMatrices(xeqyref_matrix, triangle_matrix, 3, 3, 3);
     
     for(int i=0; i<3; i++)  {
-        glVertex2d(xref_triangle[0][i], xref_triangle[1][i]);
+        glVertex2d((int)xref_triangle[0][i], (int)xref_triangle[1][i]);
     }
     for(int i=0; i<3; i++)  {
-        glVertex2d(yref_triangle[0][i], yref_triangle[1][i]);
+        glVertex2d((int)yref_triangle[0][i], (int)yref_triangle[1][i]);
     }
     for(int i=0; i<3; i++)  {
-        glVertex2d(xyref_triangle[0][i], xyref_triangle[1][i]);
+        glVertex2d((int)xyref_triangle[0][i], (int)xyref_triangle[1][i]);
     }
     for(int i=0; i<3; i++)  {
-        glVertex2d(xeqyref_triangle[0][i], xeqyref_triangle[1][i]);
+        glVertex2d((int)xeqyref_triangle[0][i], (int)xeqyref_triangle[1][i]);
     }
     glEnd();
 }
@@ -202,9 +252,14 @@ void display_transforms()   {
     // scanf("%d %d", &xs[2], &ys[2]);
     glColor3f(0.0, 0.0, 1.0);
     plotTriangle(xs, ys);
-    // plotTranslatedTriangle(xs, ys, -100, -50);
     glColor3f(1.0, 0.0, 0.0);
-    plotReflectedTriangle(xs, ys);
+    // plotTranslatedTriangle(xs, ys, -100, -50);
+    int x0 = -50;
+    int y0 = -40;
+    int theta = -30;
+    // plotRotatedMatrix(xs, ys, 0, 0, theta);
+    plotRotatedTriangle(xs, ys, x0, y0, theta);
+    // plotReflectedTriangle(xs, ys);
 
     glFlush();
 }
@@ -226,7 +281,9 @@ int main(int argc, char **argv)  {
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(640, 480);
 
-    glutCreateWindow("Ex5A - 2D Translation");
+    // glutCreateWindow("Ex5A - 2D Translation");
+    glutCreateWindow("Ex5A - 2D Rotation");
+    // glutCreateWindow("Ex5D - 2D Reflection");
     glutDisplayFunc(display_transforms);
 
     init();
